f5676788e1a3c2545c35d475c5cb2369da723f15
Sava Mikalacki <mikalackis@gmail.com>
2016-08-26 16:57:08 +0200

    Field cant be private or protected for gson

--- arielguardianlibrary/src/main/java/com/ariel/guardian/library/commands/Params.java
+++ arielguardianlibrary/src/main/java/com/ariel/guardian/library/commands/Params.java
@@ -3,12 +3,12 @@ package com.ariel.guardian.library.commands;
 /**
  * Marker interface for command parameters
  */
-public abstract class Params {
+public class Params {
 
     /**
      * Type of Params class implementation, used by RuntimeTypeAdapterFactory and GSON
       */
-    protected String type;
+    public String type;
 
     protected Params(final String type){
         this.type = type;
